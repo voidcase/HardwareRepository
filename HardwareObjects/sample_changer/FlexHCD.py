0fc2ec7c635234d33f4215834d815d008ad1e553
bliss administrator <blissadm@id29control.esrf.fr>
2018-01-21 13:56:00 +0100

    sample_changer/FlexHCD: added call to 'prepare_flex' in _doUnload

--- HardwareObjects/sample_changer/FlexHCD.py
+++ HardwareObjects/sample_changer/FlexHCD.py
@@ -36,7 +36,7 @@ class Basket(Container):
             slot = Pin(self,cell_no,basket_no,i+1)
             self._addComponent(slot)
         self.present = True
-                            
+ 
     @staticmethod
     def getBasketAddress(cell_number,basket_number):
         return str(cell_number)+":"+str(basket_number)
@@ -63,7 +63,7 @@ class Cell(Container):
       else:
         for i in range(3):
           self._addComponent(Basket(self,number,i+1, unipuck=True))
-       
+
     @staticmethod
     def getCellAddress(cell_number):
       return str(cell_number)
@@ -235,9 +235,7 @@ class FlexHCD(SampleChanger):
     def get_gripper(self):
         gripper_type = {1:"UNIPUCK", 3:"SPINE"}
         tt = self._execute_cmd("get_gripper_type")
-        if tt in gripper_type:
-            return gripper_type[tt]
-        return "?"
+        return gripper_types.get(gripper_type, "?")
 
     @task
     def change_gripper(self):
@@ -289,6 +287,8 @@ class FlexHCD(SampleChanger):
         self._execute_cmd('unloadSample', sample.getCellNo(), sample.getBasketNo(), sample.getVialNo())
         if self._execute_cmd("get_loaded_sample") == (-1,-1,-1):
             self._resetLoadedSample()
+            if self.controller:
+                self.controller.prepare_flex(release_interlock=True)
             return True
         return False
 
