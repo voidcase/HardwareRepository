5398d455b8000c1f63e68137340b57da5e640c49
Mikel Eguiraun <mikegu@b-v-biomax-web-0.maxiv.lu.se>
2018-03-08 13:38:41 +0100

    [min, max] values for exp_time

--- HardwareObjects/BeamlineSetup.py
+++ HardwareObjects/BeamlineSetup.py
@@ -303,6 +303,13 @@ class BeamlineSetup(HardwareObject):
 
         try:
             exp_time_limit = self[parent_key].getProperty('exposure_time')
+	    if type(exp_time_limit) != int:
+                # min and max specified <exp_time>0.01,500</exp_time>
+                exp_time_limit = exp_time_limit.split(',')
+            else:
+                # only max specified <exp_time>500</exp_time>
+                exp_time_limit = [0, exp_time_limit] #always min value
+
             if exp_time_limit is not None:
                 limits['exposure_time'] = exp_time_limit
         except:
